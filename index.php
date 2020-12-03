e9b89ee5cf12418265d34e8cf95dd469cfe99a32
bismipoulose999 <bismipoulose999@gmail.com>
2020-12-03 17:11:53 +0530

    css added

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 </head>
-<body>
-    <h1>Library App</h1>
+<body class="bgcolor">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Library App</h1>
     <table>
         <tr>
             <td>Book Name</td>
@@ -35,4 +41,7 @@
     </table>
     
 </body>
+            </div>
+        </div>
+    </div>
 </html>
